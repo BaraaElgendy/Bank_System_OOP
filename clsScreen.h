ece3122f384b700630915aeb10ebfa521d2bfe6a
Baraa Elgendy <baraagendy23@gmail.com>
2026-08-11 23:43:09 +0300

    Extention-01 Added Logged In User And System Date In All Screens

--- clsScreen.h
+++ clsScreen.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<iostream>
+#include"clsDate.h"
 #include"clsUser.h"
 #include"Global.h"
 using namespace std;
@@ -8,6 +9,14 @@ class clsScreen
 
 protected:
 
+	static void _ShowDateAndUser() {
+		
+		cout << "\t\t\t\tUser: " << CurrentUser.UserName() <<endl;
+		cout << "\t\t\t\tDate: ";
+		clsDate::GetSystemDate().Print();
+
+	}
+
 	static void _DrawScreenHeader(string Title, string Subtitle = " ") {
 		cout << "\t\t\t\t----------------------------------------------------\n\n";
 		cout << "\t\t\t\t\t" << Title << endl;
@@ -15,8 +24,8 @@ protected:
 			cout << "\n\t\t\t\t\t" << Subtitle << endl;
 		}
 		cout << "\n\t\t\t\t----------------------------------------------------\n\n";
-
-
+		_ShowDateAndUser();
+		cout << "\n\n";
 	}
 
 	static bool CheckAccessRights(clsUser::enPermessions Permession) {
